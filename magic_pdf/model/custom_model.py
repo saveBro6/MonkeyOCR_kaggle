264fbb8504cc3d9f7ff9bf0925ea7c6a4a4d95de
hungmtp <hungmtp1309@gmail.com>
2025-11-22 02:30:15 +0700

    try device map again

--- magic_pdf/model/custom_model.py
+++ magic_pdf/model/custom_model.py
@@ -248,16 +248,14 @@ class MonkeyChat_transformers:
         self.max_batch_size = max_batch_size
         self.max_new_tokens = max_new_tokens
         
+        if torch.cuda.device_count() > 1:
+            self.device_map = "auto"   
+
         if device is None:
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-            
-            if torch.cuda.device_count() > 1:
-                self.device_map = "auto"   
-            else:
-                self.device_map = None
+        
         else:
             self.device = device
-            self.device_map = None
         
         bf16_supported = False
         if self.device.startswith("cuda"):
@@ -274,7 +272,7 @@ class MonkeyChat_transformers:
                         model_path,
                         torch_dtype=torch.bfloat16 if bf16_supported else torch.float16,
                         attn_implementation="eager" if self.device.startswith("cuda") else 'sdpa',
-                        device_map=self.device_map,
+                        device_map=self.device,
                     )
                 
             self.processor = AutoProcessor.from_pretrained(
